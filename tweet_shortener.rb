2a02b2bd06909c0351491853c3cf7724c7ffd07f
hao wang <ge_nah@hotmail.com>
2019-05-13 21:55:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,51 @@
 # Write your code here.
+def dictionary
+  hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "For" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  array = tweet.split(" ").
+  new_array = []
+  array.each do |word|
+    if dictionary.keys.include?(word)
+      new_array << dictionary[word]
+    else
+      new_array << word
+    end
+  end
+  new_array.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+  array.each do |sent|
+    puts word_substituter(sent)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length < 140
+    tweet
+  else
+    word_substituter(tweet)
+  end
+end 
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    tweet[0...137] + "..."
+  else
+    word_substituter(tweet)  
+  end
+end
